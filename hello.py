f78e3a6feb32886c97949c78af6ef68d436f8b0b
Asutosh Panda <panda.asutosh04@gmail.com>
2020-01-04 14:24:36 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return '{  
+    "employee": {  
+        "name":       "Asutosh",   
+        "salary":      -500,   
+        "married":    "to-be"  
+    }  
+}'
 
 if __name__ == '__main__':
     app.run()
