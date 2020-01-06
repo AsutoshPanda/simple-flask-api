050415a7756de89ad63db2af5b6ad36e30add94b
Asutosh Panda <panda.asutosh04@gmail.com>
2020-01-06 17:24:06 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -27,7 +27,7 @@ def respond():
         response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
 
     # Return the response in json format
-    return jsonify(response)
+    return print_final_verdict()
 
 @app.route('/post/', methods=['POST'])
 def post_something():
@@ -53,3 +53,77 @@ def index():
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
+    
+    
+# face verification with the VGGFace2 model
+from matplotlib import pyplot
+from PIL import Image
+from numpy import asarray
+from scipy.spatial.distance import cosine
+from mtcnn import MTCNN
+from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
+ 
+# extract a single face from a given photograph
+def extract_face(filename, required_size=(224, 224)):
+	# load image from file
+	pixels = pyplot.imread(filename)
+	# create the detector, using default weights
+	detector = MTCNN()
+	# detect faces in the image
+	results = detector.detect_faces(pixels)
+	# extract the bounding box from the first face
+	x1, y1, width, height = results[0]['box']
+	x2, y2 = x1 + width, y1 + height
+	# extract the face
+	face = pixels[y1:y2, x1:x2]
+	# resize pixels to the model size
+	image = Image.fromarray(face)
+	image = image.resize(required_size)
+	face_array = asarray(image)
+	return face_array
+ 
+# extract faces and calculate face embeddings for a list of photo files
+def get_embeddings(filenames):
+	# extract faces
+	faces = [extract_face(f) for f in filenames]
+	# convert into an array of samples
+	samples = asarray(faces, 'float32')
+	# prepare the face for the model, e.g. center pixels
+	samples = preprocess_input(samples, version=2)
+	# create a vggface model
+	model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+	# perform prediction
+	yhat = model.predict(samples)
+	return yhat
+ 
+# determine if a candidate face is a match for a known face
+def is_match(known_embedding, candidate_embedding, thresh=0.5):
+	# calculate distance between embeddings
+	score = cosine(known_embedding, candidate_embedding)
+	if score <= thresh:
+		# print('>face is a Match (%.3f <= %.3f)' % (score, thresh))
+                       return True
+	else:
+		# print('>face is NOT a Match (%.3f > %.3f)' % (score, thresh))
+         return False
+
+import urllib.request
+
+def store_image(url, local_file_name):
+  with urllib.request.urlopen(url) as resource:
+    with open(local_file_name, 'wb') as f:
+      f.write(resource.read())
+    
+def print_final_verdict():
+    store_image('https://m.media-amazon.com/images/M/MV5BMTkxMzk4MjQ4MF5BMl5BanBnXkFtZTcwMzExODQxOA@@._V1_.jpg','iacocca_1.jpg')
+    store_image('https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTM2NTg1NzQz/tom-cruise-9262645-1-402.jpg','iacocca_2.jpg')
+    filenames = ['iacocca_1.jpg', 'iacocca_2.jpg']    
+    embeddings = get_embeddings(filenames)
+    if is_match(embeddings[0], embeddings[1]):
+        return 'matched'
+    else:
+        return 'unmatched'
+    
+
+        
